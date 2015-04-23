c01d698a93ae17f98474440a75824753edf01f5c
Chris Frisz <chris.frisz@gmail.com>
2015-04-23 08:50:21 -0400

    Adding first cut of card-game-war kata solution.
    Completed in 44:08.

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,28 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn play-round [player1-card player2-card]
+  (let [[suit1 rank1] player1-card
+        [suit2 rank2] player2-card]
+    (if (= rank1 rank2)
+        (let [suit1-idx (.indexOf suits suit1)
+              suit2-idx (.indexOf suits suit2)]
+          (if (> suit1-idx suit2-idx)
+              player1-card
+              player2-card))
+        (let [rank1-idx (.indexOf ranks rank1)
+              rank2-idx (.indexOf ranks rank2)]
+          (if (> rank1-idx rank2-idx) 
+            player1-card
+            player2-card)))))
 
-(defn play-game [player1-cards player2-cards])
+(defn play-game [player1-cards player2-cards]
+  (if (or (nil? (seq player1-cards))
+          (nil? (seq player2-cards)))
+      [player1-cards player2-cards]
+      (let [[p1-card player1-cards] ((juxt first (comp vec rest)) player1-cards) 
+            [p2-card player2-cards] ((juxt first (comp vec rest)) player2-cards) 
+            winner (play-round p1-card p2-card)]
+        (if (= winner p1-card)
+            (recur (conj player1-cards p1-card p2-card) player2-cards)
+            (recur player1-cards (conj player2-cards p1-card p2-card))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,14 +6,52 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= (play-round [:spades :ace] [:club 10])
+           [:spades :ace]))
+    (is (= (play-round [:diamond 5] [:heart 9])
+           [:heart 9])))
+  (testing "queens are higher rank than jacks"
+    (is (= (play-round [:heart :queen] [:club :jack])
+           [:heart :queen]))
+    (is (= (play-round [:club :jack] [:heart :queen])
+           [:heart :queen])))
+  (testing "kings are higher rank than queens"
+    (is (= (play-round [:heart :queen] [:club :king])
+           [:club :king]))
+    (is (= (play-round [:club :king] [:heart :queen])
+           [:club :king])))
+  (testing "aces are higher rank than kings"
+    (is (= (play-round [:heart :ace] [:club :king])
+           [:heart :ace]))
+    (is (= (play-round [:club :king] [:heart :ace])
+           [:heart :ace])))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= (play-round [:club :jack] [:spade :jack])
+           [:club :jack]))
+    (is (= (play-round [:spade :jack] [:club :jack])
+           [:club :jack])))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= (play-round [:club :jack] [:diamond :jack])
+           [:diamond :jack]))
+    (is (= (play-round [:diamond :jack] [:club :jack])
+           [:diamond :jack])))
+  (testing "if the ranks are equal, hearts beat diamonds")
+    (is (= (play-round [:heart :jack] [:diamond :jack])
+           [:heart :jack]))
+    (is (= (play-round [:heart :jack] [:club :jack])
+           [:heart :jack])))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (let [player1-cards (subvec (vec cards) 0 (/ (count (vec cards)) 2))
+          ;; player 2 gets all the good cards
+          player2-cards (subvec (vec cards) (/ (count (vec cards)) 2))
+          [player1-cards player2-cards] (play-game player1-cards player2-cards)]
+      (is (nil? (seq player1-cards)))
+      (is (= (count player2-cards) (count cards))))
+    (let [player1-cards (subvec (vec cards) (/ (count (vec cards)) 2))
+          player2-cards (subvec (vec cards) 0 (/ (count (vec cards)) 2))
+          [player1-cards player2-cards] (play-game player1-cards player2-cards)]
+      (is (nil? (seq player2-cards)))
+      (is (= (count player1-cards) (count cards))))))
 

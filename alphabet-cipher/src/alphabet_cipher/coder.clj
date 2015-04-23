0860e919895a9afb7a009dd29c5e5665c74bca02
Chris Frisz <chris.frisz@gmail.com>
2015-04-22 22:43:41 -0400

    Adding first cut of "Alphabet Cipher" kata.
    Completed in 51:27.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,32 @@
 (ns alphabet-cipher.coder)
 
 (defn encode [keyword message]
-  "encodeme")
+  (letfn [(encode-letter [c1 c2]
+            (let [off (int \a)]
+              (char (+ (mod (apply + (mapv #(- % off) (mapv int [c1 c2]))) 26) off))))]
+    (loop [kidx 0
+           midx 0
+           encrypted ""]
+      (if (= midx (count message))
+          encrypted
+          (recur (mod (inc kidx) (count keyword))
+            (inc midx)
+            (str encrypted 
+              (encode-letter (get keyword kidx)
+                (get message midx))))))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (letfn [(decode-letter [k m]
+            (let [kint (- (int k) (int \a))
+                  mint (- (int m) (int \a))]
+              (char (+ (mod (- mint kint) 26) (int \a)))))]
+    (loop [kidx 0
+           midx 0
+           decrypted ""]
+      (if (= midx (count message))
+          decrypted
+          (recur (mod (inc kidx) (count keyword))
+            (inc midx)
+            (str decrypted
+              (decode-letter (get keyword kidx)
+                (get message midx))))))))

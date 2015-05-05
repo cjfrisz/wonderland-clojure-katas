1f40170aee0ce369f64160dfda05bb9170f9281b
Chris Frisz <chris.frisz@gmail.com>
2015-05-05 09:08:12 -0400

    Adding more systematic solution for magic square.

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -2,10 +2,6 @@
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
-(defn row-sums-equal?
-  [square]
-  (apply = (map (partial reduce +) square)))
-
 (defn column-sums-equal?
   [square]
   (= (reduce + (map first square))
@@ -21,17 +17,25 @@
         (get-in square [1 1])
         (get-in square [0 2]))))
 
-(defn magic-square?
-  [square]
-  (and (row-sums-equal? square)
-       (column-sums-equal? square)
-       (diagonal-sums-equal? square)))
+(defn magic-square
+  [values]
+  (let [set-average (/ (reduce + values) (count values))
+        row* (for [x values
+                   y values
+                   z values
+                   :when (and (not (or (= x y)
+                                       (= y z)
+                                       (= x z)))
+                              (= (/ (+ x y z) 3)
+                                 set-average))]
+               [x y z])]
+    (first (take 1 (for [row1 row*
+                         row2 row*
+                         row3 row*
+                         :let [square [row1 row2 row3]]
+                         :when (and (= (set (concat row1 row2 row3))
+                                       (set values))
+                                    (column-sums-equal? square)
+                                    (diagonal-sums-equal? square))]
+                        square)))))
 
-;; random!
-(defn magic-square [values]
-  (letfn [(random-square [values]
-            (vec (map vec (partition 3 (shuffle values)))))]
-    (loop [square (random-square values)]
-      (if (magic-square? square)
-          square
-          (recur (random-square values))))))

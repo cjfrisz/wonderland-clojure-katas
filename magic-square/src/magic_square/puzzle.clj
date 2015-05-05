bd24f7ce91681603ea7228d27f31bdb8353bf05d
Chris Frisz <chris.frisz@gmail.com>
2015-05-05 08:37:49 -0400

    Adding magic-square solution.
    Having a bit of fun, I implemented a solution that shuffles the input set
    and partitions the result repeatedly until it happens upon a magic-square.
    Most of the work went into re-implementing the predicates for checking
    whether a given square is magic.
    
    Very silly solution that doesn't have predictable performance
    characteristics, but it seemed like the fun thing to do.
    
    Completed in 33:50.

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -2,7 +2,36 @@
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
+(defn row-sums-equal?
+  [square]
+  (apply = (map (partial reduce +) square)))
+
+(defn column-sums-equal?
+  [square]
+  (= (reduce + (map first square))
+     (reduce + (map second square))
+     (reduce + (map last square))))
+
+(defn diagonal-sums-equal?
+  [square]
+  (= (+ (get-in square [0 0]) 
+        (get-in square [1 1])
+        (get-in square [2 2]))
+     (+ (get-in square [2 0]) 
+        (get-in square [1 1])
+        (get-in square [0 2]))))
+
+(defn magic-square?
+  [square]
+  (and (row-sums-equal? square)
+       (column-sums-equal? square)
+       (diagonal-sums-equal? square)))
+
+;; random!
 (defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]])
+  (letfn [(random-square [values]
+            (vec (map vec (partition 3 (shuffle values)))))]
+    (loop [square (random-square values)]
+      (if (magic-square? square)
+          square
+          (recur (random-square values))))))

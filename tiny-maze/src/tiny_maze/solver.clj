9c70c6a1e75589aa5061df36b430de8194302b73
Chris Frisz <chris.frisz@gmail.com>
2015-05-07 08:58:38 -0400

    Adding solution for tiny-maze kata.
    Completed in 51:57.

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,32 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(defn exit? [maze [y x]]
+  (and (= x (dec (count (first maze))))
+       (= y (dec (count maze)))))
+
+(defn next-moves [maze [y x]]
+  (for [[next-y next-x] [[y (dec x)] [(inc y) x] [y (inc x)] [(dec y) x]]
+        :when (and (<= 0 next-x (dec (count (first maze))))
+                   (<= 0 next-y (dec (count maze)))
+                   (some #{(get-in maze [next-y next-x])} [0 :E]))]
+       [next-y next-x]))
+
+(defn solve-maze [maze]
+  (loop [coords [0 0]
+         maze (assoc-in maze [0 0] :x)
+         alt-move* nil
+         prev-maze nil]
+        (if (exit? maze coords)
+            maze
+            (let [move* (next-moves maze coords)]
+              (if (nil? (seq move*))
+                  (let [alt-move (first alt-move*)]
+                    (recur alt-move
+                           (assoc-in prev-maze alt-move :x)
+                           (next alt-move*)
+                           nil))
+                  (let [move (first move*)]
+                    (recur move
+                           (assoc-in maze move :x)
+                           (next move*)
+                           maze)))))))

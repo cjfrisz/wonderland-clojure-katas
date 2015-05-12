9ff8cacbd8caa57bd773c4428f6518ea2f3f0f11
Chris Frisz <chris.frisz@gmail.com>
2015-05-12 08:40:53 -0400

    Adding solution for wonderland-number.
    Initially toyed with calculating the number by hand for a few minutes before
    realizing that there was an incredibly simple list comprehension solution
    using "for."
    
    Completed in 10:38.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,15 @@
 (ns wonderland-number.finder)
 
+(defn hasAllTheSameDigits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (for [n (range 100000 999999)
+               :when (and (hasAllTheSameDigits? n (* n 2))
+                          (hasAllTheSameDigits? n (* n 3))
+                          (hasAllTheSameDigits? n (* n 4))
+                          (hasAllTheSameDigits? n (* n 5))
+                          (hasAllTheSameDigits? n (* n 6)))] 
+           n)))
